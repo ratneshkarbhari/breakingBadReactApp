82c1cc86e8eb79868bf92be0fa81429ee6dc6449
Ratnesh <rkarbhari23@gmail.com>
2020-10-13 01:28:21 +0530

    Saving repo

---
+++ src/components/characters/CharacterGrid.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CharacterItem from './CharacterItem'
+import Spinner from './Spinner'
+
+const CharacterGrid = ({items,isLoading}) => {
+    return isLoading ? (<Spinner />) : <section className="cards">
+        {items.map((item) => (
+            <CharacterItem key={items.char_id} item={item} />
+        ))}
+    </section>
+}
+
+export default CharacterGrid
---
+++ src/components/ui/Search.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react'
+
+const Search = ({getQuery}) => {
+
+    const [text, setText] = useState('')
+
+    const onChange = (q) => {
+        setText(q)
+        getQuery(q)
+    }
+
+    return (
+        <section className="search">
+            <form>
+                <input type="text"
+                onChange = {(e) => onChange(e.target.value)}
+                placeholder="Search Characters"
+                autoFocus
+                />
+            </form>
+        </section>
+    )
+}
+
+export default Search
